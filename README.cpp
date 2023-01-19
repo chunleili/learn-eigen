1d12fc6408607b458dd736d1873b6c79b3f5fba0
chunleili <li_cl@foxmail.com>
2023-01-19 22:43:10 +0800

    更新README并测试遍历和填充

--- README.cpp
+++ README.cpp
@@ -4,7 +4,12 @@
 将每个main_xxx.cpp文件改为main.cpp，然后编译运行即可。
 
 目前测试了
-- BiCGStab.cpp
+- BiCGStab(一种迭代求解器)
+- Cholesky(一种分解求解器)
+- fillSparseMatrix(填充稀疏矩阵)
+- traverseSparseMatrix(遍历稀疏矩阵)
+- matrixFree(无矩阵迭代求解器)
+- SparseMatrix1(稀疏矩阵求解Laplace问题的案例, 采用SparseCholesky)
 
 ## data
 data文件夹下面的是matrix market格式的矩阵数据。
